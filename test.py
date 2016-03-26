2688d9b6ae892b941832eeeba21ba51769926b0f
David Townshend <townshend.dr@gmail.com>
2016-03-26 14:37:01 +0200

    Rewrite tests using urllib instead of curl

--- test.py
+++ test.py
@@ -2,31 +2,55 @@
 
 import subprocess
 import unittest
+from urllib.request import Request, urlopen
 
-curl_args = [
-    '-X', 'POST',
-    '-d', '@test.html',
-    '--header', 'Content-Type:application/html',
-]
+html_data = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test File</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
+</html>
+'''.encode('utf-8')
 
-host = "http://127.0.0.1:5001"
+
+import urllib.request
+req = urllib.request.Request('http://www.example.com/')
+req.add_header('Referer', 'http://www.python.org/')
+r = urllib.request.urlopen(req)
+
+
+def request_factory(path='/'):
+    url = 'http://127.0.0.1:5001%s' % path
+    headers = {
+        'Content-Type': 'application/html'
+    }
+    return Request(url, data=html_data, headers=headers, method='POST')
+
 
 class TestAPI(unittest.TestCase):
 
+    def setUp(self):
+        request = request_factory('/sample')
+        self.response = urllib.request.urlopen(request)
+
+    def tearDown(self):
+        self.response.close()
+
+    def test_response_code(self):
+        self.assertEqual(self.response.getcode(), 200)
+
     def test_headers(self):
-        cmd = ['curl', '-I'] + curl_args + [host + '/sample']
-        got_headers = subprocess.run(curl)
-        expect_headers = [
+        self.assertEqual(self.info().headers, [
             'Content-Type: application/pdf',
             'Content-Disposition: inline; filename=sample.pdf'
-        ]
-        for expected in expect_headers:
-            self.assertEqual(got_headers, expected)
+        ])
 
     def test_body(self):
-        cmd = ['curl'] + curl_args + [host]
-        body = subprocess.run(cmd)
-        self.assertEqual(body[:4], b'%PDF')
+        self.assertEqual(response.read()[:4], b'%PDF')
 
 
 if __name__ == '__main__':
